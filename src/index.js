b6deef265f9fb5d8ad51b71c172ef1722dd1243a
John Farrell <johnefarrell18@gmail.com>
2020-09-13 19:12:12 -0700

    scrub

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function createHeading() {
 
 function createDistort() {
     const root = document.querySelector('.heading');
-    const canvas = document.querySelector('.heading__canvas');
+    const canvas = document.getElementById('heading__canvas');
     const context = canvas.getContext('2d');
     const img = new Image();
     img.src = forest.default;
@@ -28,12 +28,12 @@ function createDistort() {
     }
 }
 
-function isVisible(el) {
+function isVisible(el, divisor = 1.5) {
     const { top, bottom } = el.getBoundingClientRect();
     const vHeight = (window.innerHeight || document.documentElement.clientHeight);
     return (
       (top > 0 || bottom > 0) &&
-      top < (vHeight / 1.5)
+      top < (vHeight / divisor)
     );
 }
 
@@ -47,9 +47,39 @@ function observe() {
         })
     })
 }
+
+function scrub(vid) {
+    const duration = 1/100;
+    const frameNumber  = vid.getBoundingClientRect().top;
+    if (frameNumber > 0) {
+        vid.currentTime = Math.abs(frameNumber)/(720/375)*duration;
+    }
+}
+
+function skidScroll() {
+    const canvas = document.getElementById('video__canvas');
+    const context = canvas.getContext('2d');
+    const vid = document.querySelector('.video'); 
+    const cw = canvas.width;
+    const ch = canvas.height;
+
+    vid.addEventListener('play', () => {
+        draw(this, context, cw, ch);
+    }, false);
+
+    window.addEventListener('scroll', () => scrub(vid));
+}
+
+function draw(v,c,w,h) {
+    c.drawImage(v,0,0,w,h);
+    setTimeout(draw,20,v,c,w,h);
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     createDistort();
     createHeading();
+    skidScroll();
     observe();
 });
 
